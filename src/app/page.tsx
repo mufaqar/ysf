2ad302a74d9d1fa3334c43c3f8829d244e793bbe
mufaqar <mufaqar@gmail.com>
2024-03-07 17:25:51 +0500

    udpated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import React, { useEffect, useState } from 'react';
 async function getData() {
   const res = await fetch('https://seats.aero/partnerapi/search?origin_airport=LHR&destination_airport=EWR&take=500', {
     method: 'GET',
-    mode: 'no-cors',
     headers: {
       'Partner-Authorization': 'pro_2cIuzukk0tShnaR0KmKewgXyUZv',
     },
@@ -26,22 +25,18 @@ async function  Blog() {
   const { blogs } = await getData();
 
 
-
-
-
-
   return (
     <section className='bg-secondary/5 py-16'>
       <h2>Blog Posts</h2>
       {JSON.stringify(blogs)};
-      <ul>
-        {/* {blogs.map((blog:any, idx:number) => (
+      {/* <ul>
+        {blogs?.data.map((item:any, idx:number) => (
           <li key={idx}>
-            <p>{blog.YDirect}</p>
+            <p>ONe : {item.YAirlines}</p>
            
           </li>
-        ))} */}
-      </ul>
+        ))}
+      </ul> */}
     </section>
   );
 }
