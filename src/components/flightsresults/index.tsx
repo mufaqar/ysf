a1dbb482d5d1b6eb0c55bc1e593985933dcb4c0e
Mufaqar <mufaqar@gmail.com>
2024-03-18 15:02:10 +0500

    update

--- src/components/flightsresults/index.tsx
+++ src/components/flightsresults/index.tsx
@@ -63,7 +63,7 @@ const FinalFlightsresponse = ({ searchResult }: any) => {
             <div className="mt-8 text-center">
                 <h2 className="text-lg font-semibold mb-2">Search Results</h2>
                 <div className='flex space-x-4'>
-                    <div className='max-w-[300px] bg-red-50'>
+                    <div className='max-w-[300px] bg-[#e2e8f0] p-4 rounded-md'>
 
                         <div className=''>
                             <div className='flex space-x-2'><span className='font-bold text-red-500'>Economy</span>
@@ -79,20 +79,26 @@ const FinalFlightsresponse = ({ searchResult }: any) => {
                         {Object.keys(groupedMonths).map((month: string, index: number) => (
                             <div key={index} className=''>
 
-                                <h3 className="text-md font-semibold py-4">{month}</h3>
+                                <h3 className="text-md font-semibold py-4 text-left">{month}</h3>
                                 <div className='py-4 leading-[30px] '>
-                                    {Object.keys(groupedMonths[month]).map((day: string, dayIndex: number) => (
-                                        <span key={dayIndex} className='px-[4px]'>
+                                    {Object.keys(groupedMonths[month]).map((day: string, dayIndex: number) =>{
+                                        return(
+                                            <>
+                                             {groupedMonths[month][day]>0 && (
+                                            <span key={dayIndex} className='px-[4px]'>
                                             {day} ({groupedMonths[month][day]})
                                             {dayIndex < Object.keys(groupedMonths[month]).length - 1 && '|'}
                                         </span>
-                                    ))}
+                                        )}
+                                            </>
+                                        )
+                                    })}
                                 </div>
                             </div>
                         ))}
                     </div>
 
-                    <div className='max-w-[300px]'>
+                    <div className='max-w-[300px] bg-[#f1f5f9] p-4 rounded-md'>
 
                         <div className=''>
                             <div className='flex space-x-2'><span className='font-bold text-red-500'>Bussiness</span>
@@ -108,19 +114,25 @@ const FinalFlightsresponse = ({ searchResult }: any) => {
                         {Object.keys(jmonth).map((month: string, index: number) => (
                             <div key={index} className=''>
 
-                                <h3 className="text-md font-semibold py-4">{month}</h3>
+                                <h3 className="text-md font-semibold py-4 text-left">{month}</h3>
                                 <div className='py-4 leading-[30px] '>
-                                    {Object.keys(jmonth[month]).map((day: string, dayIndex: number) => (
-                                        <span key={dayIndex} className='px-[4px]'>
+                                    {Object.keys(jmonth[month]).map((day: string, dayIndex: number) =>{
+                                        return(
+                                            <>
+                                             {jmonth[month][day]>0 && (
+                                            <span key={dayIndex} className='px-[4px]'>
                                             {day} ({jmonth[month][day]})
                                             {dayIndex < Object.keys(jmonth[month]).length - 1 && '|'}
                                         </span>
-                                    ))}
+                                        )}
+                                            </>
+                                        )
+                                    })}
                                 </div>
                             </div>
                         ))}
                     </div>
-                    <div className='max-w-[300px]'>
+                    <div className='max-w-[300px] bg-[#bbf7d0] p-4 rounded-md'>
 
                         <div className=''>
                             <div className='flex space-x-2'><span className='font-bold text-red-500'>Economy Premium</span>
@@ -136,20 +148,26 @@ const FinalFlightsresponse = ({ searchResult }: any) => {
                         {Object.keys(wmonth).map((month: string, index: number) => (
                             <div key={index} className=''>
 
-                                <h3 className="text-md font-semibold py-4">{month}</h3>
+                                <h3 className="text-md font-semibold py-4 text-left">{month}</h3>
                                 <div className='py-4 leading-[30px] '>
-                                    {Object.keys(wmonth[month]).map((day: string, dayIndex: number) => (
-                                        <span key={dayIndex} className='px-[4px]'>
+                                    {Object.keys(wmonth[month]).map((day: string, dayIndex: number) =>{
+                                        return(
+                                            <>
+                                             {wmonth[month][day]>0 && (
+                                            <span key={dayIndex} className='px-[4px]'>
                                             {day} ({wmonth[month][day]})
                                             {dayIndex < Object.keys(wmonth[month]).length - 1 && '|'}
                                         </span>
-                                    ))}
+                                        )}
+                                            </>
+                                        )
+                                    })}
                                 </div>
                             </div>
                         ))}
                     </div>
 
-                    <div className='max-w-[300px]'>
+                    <div className='max-w-[300px] bg-[#dcfce7] p-4 rounded-md'>
 
                         <div className=''>
                             <div className='flex space-x-2'><span className='font-bold text-red-500'>Economy Premium</span>
@@ -165,14 +183,20 @@ const FinalFlightsresponse = ({ searchResult }: any) => {
                         {Object.keys(fmonth).map((month: string, index: number) => (
                             <div key={index} className=''>
 
-                                <h3 className="text-md font-semibold py-4">{month}</h3>
+                                <h3 className="text-md font-semibold py-4 text-left">{month}</h3>
                                 <div className='py-4 leading-[30px] '>
-                                    {Object.keys(fmonth[month]).map((day: string, dayIndex: number) => (
-                                        <span key={dayIndex} className='px-[4px]'>
+                                    {Object.keys(fmonth[month]).map((day: string, dayIndex: number) =>{
+                                        return(
+                                            <>
+                                             {fmonth[month][day]>0 && (
+                                            <span key={dayIndex} className='px-[4px]'>
                                             {day} ({fmonth[month][day]})
                                             {dayIndex < Object.keys(fmonth[month]).length - 1 && '|'}
                                         </span>
-                                    ))}
+                                        )}
+                                            </>
+                                        )
+                                    })}
                                 </div>
                             </div>
                         ))}
