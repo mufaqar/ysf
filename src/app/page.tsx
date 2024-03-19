4dc6dd3699391305a0ffde97ca180df53e0176e5
Mufaqar <mufaqar@gmail.com>
2024-03-19 15:56:55 +0500

    ipdate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,52 @@
-
-import Filters from '@/components/filters';
+import Filters from './filters/page';
 import React from 'react';
+
 async function getData() {
-    const res = await fetch('https://seats.aero/partnerapi/availability?take=500', {
-        method: 'GET',
-        headers: {
-            'Partner-Authorization': 'pro_2cIuzukk0tShnaR0KmKewgXyUZv',
-        },
-    });
-    if (!res.ok) {
-        throw new Error('Failed to fetch data');
+    try {
+        const res = await fetch('https://seats.aero/partnerapi/availability?take=5000', {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                'Partner-Authorization': 'pro_2cIuzukk0tShnaR0KmKewgXyUZv',
+            },
+        });
+        
+        if (!res.ok) {
+            throw new Error('Failed to fetch data. Status: ' + res.status);
+        }
+        
+        const AiroResponse = await res.json();
+
+        const originsSet = new Set<string>(AiroResponse.data.map((item: any) => item.Route.OriginAirport));
+        const destinationsSet = new Set<string>(AiroResponse.data.map((item: any) => item.Route.DestinationAirport));
+
+        const origins: string[] = [];
+        const destinations: string[] = [];
+
+        originsSet.forEach((origin: string) => origins.push(origin));
+        destinationsSet.forEach((destination: string) => destinations.push(destination));
+
+        return {
+            origins,
+            destinations,
+            AiroResponse,
+        };
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error; // re-throw the error to propagate it up
     }
-    const AiroResponce = await res.json();
-    const originsSet = new Set<string>(AiroResponce.data.map((item: any) => item.Route.OriginAirport));
-    const destinationsSet = new Set<string>(AiroResponce.data.map((item: any) => item.Route.DestinationAirport));
-
-    const origins: string[] = [];
-    const destinations: string[] = [];
-
-    originsSet.forEach((origin: string) => origins.push(origin));
-    destinationsSet.forEach((destination: string) => destinations.push(destination));
-
-    return {
-        origins,
-        destinations
-    };
 }
+
 const BlogPage = async () => {
-    const { origins, destinations } = await getData()
+    const { origins, destinations } = await getData();
 
     return (
         <>
-            <Filters origins={origins} destination={destinations} />
+            {/* {JSON.stringify(AiroResponce.data[0])} */}
+
+            <Filters origins={origins} destinations={destinations} />
         </>
-    )
-}
+    );
+};
+
 export default BlogPage;
--- src/components/finalshowingflights/index.tsx
+++ src/components/finalshowingflights/index.tsx
@@ -1,49 +1,210 @@
-import React from 'react'
+import React from 'react';
 
 const FinalFlightsresponse = ({ searchResult }: any) => {
+    // Extract OriginAirport and DestinationAirport
+    const originAirport = searchResult.length > 0 ? searchResult[0].Route.OriginAirport : '';
+    const destinationAirport = searchResult.length > 0 ? searchResult[0].Route.DestinationAirport : '';
+    const source = searchResult.length > 0 ? searchResult.Route.Source : '';
+    const distance = searchResult.length > 0 ? searchResult[0].Route.Distance : '';
+    const yairlines = searchResult.length > 0 ? searchResult[0].YAirlines : '';
+    const jAirlines = searchResult.length > 0 ? searchResult[0].JAirlines : '';
+    const wAirlines = searchResult.length > 0 ? searchResult[0].WAirlines : '';
+    const fAirlines = searchResult.length > 0 ? searchResult[0].FAirlines : '';
+    const groupedMonths: { [key: string]: { [key: string]: number } } = {};
+    searchResult.forEach((item: any) => {
+        const date = new Date(item.Date);
+        const month = date.toLocaleString('en-US', { month: 'short' });
+        const day = date.getDate().toString();
 
-     return (
-          <>
-               {/* Show Search Results */}
+        if (!groupedMonths[month]) {
+            groupedMonths[month] = {};
+        }
+        groupedMonths[month][day] = (groupedMonths[month][day] || 0) + item.YRemainingSeats;
+    });
 
-               <div className="mt-8 text-center">
-                    <h2 className="text-lg font-semibold mb-2">Search Results</h2>
-                    <table className="border-collapse border border-gray-400 m-auto">
-                         <thead>
-                              <tr>
-                                   <th className="border border-gray-400 px-4 py-2">Depart</th>
-                                   <th className="border border-gray-400 px-4 py-2">Arrive</th>
 
-                                   <th className="border border-gray-400 px-4 py-2">Source</th>
-                                   <th className="border border-gray-400 px-4 py-2">Bussiness</th>
-                                   <th className="border border-gray-400 px-4 py-2">Economy</th>
-                                   <th className="border border-gray-400 px-4 py-2">Premium</th>
-                                   <th className="border border-gray-400 px-4 py-2">First</th>
-                                   <th className="border border-gray-400 px-4 py-2">Remaining Seats</th>
-                                   <th className="border border-gray-400 px-4 py-2">Date</th>
-                              </tr>
-                         </thead>
-                         <tbody>
-                              {searchResult.map((item: any, index: number) => (
-                                   <tr key={index}>
-                                        <td className="border border-gray-400 px-4 py-2">{item.Route.OriginAirport}</td>
-                                        <td className="border border-gray-400 px-4 py-2">{item.Route.DestinationAirport}</td>
-                                        <td className="border border-gray-400 px-4 py-2">{item.Route.Source}</td>
-                                        <td className="border border-gray-400 px-4 py-2">{item.JMileageCost !== "0" ? item.JMileageCost : 'Not Available'}</td>
-                                        <td className="border border-gray-400 px-4 py-2">{item.YMileageCost !== "0" ? item.YMileageCost : 'Not Available'}</td>
-                                        <td className="border border-gray-400 px-4 py-2">{item.WDirectMileageCost && item.WDirectMileageCost !== "0" ? item.WDirectMileageCost : 'Not Available'}</td>
-                                        <td className="border border-gray-400 px-4 py-2">{item.FMileageCost !== "0" ? item.FMileageCost : 'Not Available'}</td>
-                                      
-                                        <td className="border border-gray-400 px-4 py-2">{item.YRemainingSeats}</td>
-                                        <td className="border border-gray-400 px-4 py-2">{item.Date}</td>
-                                   </tr>
-                              ))}
-                         </tbody>
-                    </table>
-               </div>
+    const jmonth: { [key: string]: { [key: string]: number } } = {};
+    searchResult.forEach((item: any) => {
+        const date = new Date(item.Date);
+        const month = date.toLocaleString('en-US', { month: 'short' });
+        const day = date.getDate().toString();
 
-          </>
-     )
-}
+        if (!jmonth[month]) {
+            jmonth[month] = {};
+        }
+        jmonth[month][day] = (jmonth[month][day] || 0) + item.JRemainingSeats;
+    });
 
-export default FinalFlightsresponse
+    const wmonth: { [key: string]: { [key: string]: number } } = {};
+    searchResult.forEach((item: any) => {
+        const date = new Date(item.Date);
+        const month = date.toLocaleString('en-US', { month: 'short' });
+        const day = date.getDate().toString();
+
+        if (!wmonth[month]) {
+            wmonth[month] = {};
+        }
+        wmonth[month][day] = (wmonth[month][day] || 0) + item.WRemainingSeats;
+    });
+
+    const fmonth: { [key: string]: { [key: string]: number } } = {};
+    searchResult.forEach((item: any) => {
+        const date = new Date(item.Date);
+        const month = date.toLocaleString('en-US', { month: 'short' });
+        const day = date.getDate().toString();
+
+        if (!fmonth[month]) {
+            fmonth[month] = {};
+        }
+        fmonth[month][day] = (fmonth[month][day] || 0) + item.FRemainingSeats;
+    });
+    return (
+        <>
+            <div className="mt-8 text-center">
+                <h2 className="text-lg font-semibold mb-2">Search Results</h2>
+                <div className='md:grid grid-cols-4 gap-4'>
+                    <div className='max-w-[300px] bg-[#e2e8f0] p-4 rounded-md'>
+
+                        <div className=''>
+                            <div className='flex space-x-2'><span className='font-bold text-red-500'>Economy</span>
+                                <p className='font-bold text-teal-500'>   {yairlines}</p>
+
+                            </div>
+                            <div className="flex space-x-2 pt-4 font-bold"> {distance} | {source}</div>
+                        </div>
+                        <div className='flex space-x-4'>
+                            <div className="text-md font-semibold py-4 "> {originAirport} - {destinationAirport}</div>
+                        </div>
+
+                        {Object.keys(groupedMonths).map((month: string, index: number) => (
+                            <div key={index} className=''>
+
+                                <h3 className="text-md font-semibold py-4 text-left">{month}</h3>
+                                <div className='py-4 leading-[30px] '>
+                                    {Object.keys(groupedMonths[month]).map((day: string, dayIndex: number) =>{
+                                        return(
+                                            <div key={dayIndex}>
+                                             {groupedMonths[month][day]>0 && (
+                                            <span key={dayIndex} className='px-[4px]'>
+                                            {day} ({groupedMonths[month][day]})
+                                            {dayIndex < Object.keys(groupedMonths[month]).length - 1 && '|'}
+                                        </span>
+                                        )}
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className='max-w-[300px] bg-[#f1f5f9] p-4 rounded-md'>
+
+                        <div className=''>
+                            <div className='flex space-x-2'><span className='font-bold text-red-500'>Bussiness</span>
+                                <p className='font-bold text-teal-500'>   {jAirlines}</p>
+
+                            </div>
+                            <div className="flex space-x-2 pt-4 font-bold "> {distance} | {source}</div>
+                        </div>
+                        <div className='flex space-x-4'>
+                            <div className="text-md font-semibold py-4 "> {originAirport} - {destinationAirport}</div>
+                        </div>
+
+                        {Object.keys(jmonth).map((month: string, index: number) => (
+                            <div key={index} className=''>
+
+                                <h3 className="text-md font-semibold py-4 text-left">{month}</h3>
+                                <div className='py-4 leading-[30px] '>
+                                    {Object.keys(jmonth[month]).map((day: string, dayIndex: number) =>{
+                                        return(
+                                            <div  key={dayIndex}>
+                                             {jmonth[month][day]>0 && (
+                                            <span key={dayIndex} className='px-[4px]'>
+                                            {day} ({jmonth[month][day]})
+                                            {dayIndex < Object.keys(jmonth[month]).length - 1 && '|'}
+                                        </span>
+                                        )}
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className='max-w-[300px] bg-[#bbf7d0] p-4 rounded-md'>
+
+                        <div className=''>
+                            <div className='flex space-x-2'><span className='font-bold text-red-500'>Economy Premium</span>
+                                <p className='font-bold text-teal-500'>   {wAirlines}</p>
+
+                            </div>
+                            <div className="flex space-x-2 pt-4 font-bold"> {distance} | {source}</div>
+                        </div>
+                        <div className='flex space-x-4'>
+                            <div className="text-md font-semibold py-4 "> {originAirport} - {destinationAirport}</div>
+                        </div>
+
+                        {Object.keys(wmonth).map((month: string, index: number) => (
+                            <div key={index} className=''>
+
+                                <h3 className="text-md font-semibold py-4 text-left">{month}</h3>
+                                <div className='py-4 leading-[30px] '>
+                                    {Object.keys(wmonth[month]).map((day: string, dayIndex: number) =>{
+                                        return(
+                                            <div key={dayIndex}>
+                                             {wmonth[month][day]>0 && (
+                                            <span key={dayIndex} className='px-[4px]'>
+                                            {day} ({wmonth[month][day]})
+                                            {dayIndex < Object.keys(wmonth[month]).length - 1 && '|'}
+                                        </span>
+                                        )}
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className='max-w-[300px] bg-[#dcfce7] p-4 rounded-md'>
+
+                        <div className=''>
+                            <div className='flex space-x-2'><span className='font-bold text-red-500'>First</span>
+                                <p className='font-bold text-teal-500'>   {fAirlines}</p>
+
+                            </div>
+                            <div className="flex space-x-2 pt-4 font-bold"> {distance} | {source}</div>
+                        </div>
+                        <div className='flex space-x-4'>
+                            <div className="text-md font-semibold py-4 "> {originAirport} - {destinationAirport}</div>
+                        </div>
+
+                        {Object.keys(fmonth).map((month: string, index: number) => (
+                            <div key={index} className=''>
+
+                                <h3 className="text-md font-semibold py-4 text-left">{month}</h3>
+                                <div className='py-4 leading-[30px] '>
+                                    {Object.keys(fmonth[month]).map((day: string, dayIndex: number) =>{
+                                        return(
+                                            <div key={dayIndex}>
+                                             {fmonth[month][day]>0 && (
+                                            <span key={dayIndex} className='px-[4px]'>
+                                            {day} ({fmonth[month][day]})
+                                            {dayIndex < Object.keys(fmonth[month]).length - 1 && '|'}
+                                        </span>
+                                        )}
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default FinalFlightsresponse;
